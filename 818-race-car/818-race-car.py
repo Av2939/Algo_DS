8f75e92260c2fb71a6e132c89123660126bf26b8
Av2939 <41278980+Av2939@users.noreply.github.com>
2022-08-10 01:53:13 -0400

    Time: 41 ms (98.35%), Space: 16 MB (43.35%) - LeetHub

--- 818-race-car/818-race-car.py
+++ 818-race-car/818-race-car.py
@@ -1,9 +1,9 @@
 class Solution:
     def racecar(self, target: int) -> int:
-        # Using BFS to solve solution
+        # Using the BFS approach
         
-        queue = collections.deque([(0,0,1)])
         visited = set()
+        queue = collections.deque([(0,0,1)])
         
         while queue:
             moves, position, speed = queue.popleft()
@@ -15,15 +15,12 @@ class Solution:
                 continue
             
             else:
+                
                 visited.add((position, speed))
+                queue.append((moves+1, position+speed, speed*2))
                 
-                queue.append((moves + 1, position + speed, speed *2))
-                
-                if ((position + speed > target and speed > 0) or
-                   (position + speed < target and speed < 0)):
+                if ((position + speed > target and speed > 0)
+                   or (position + speed < target and speed < 0)):
                     speed = -1 if speed > 0 else 1
                     
-                    
                     queue.append((moves + 1, position, speed))
-        
-        

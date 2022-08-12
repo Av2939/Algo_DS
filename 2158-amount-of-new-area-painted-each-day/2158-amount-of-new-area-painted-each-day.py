a218c766a79c6c8488a9036369305d063e3f87f5
Av2939 <41278980+Av2939@users.noreply.github.com>
2022-08-12 18:13:28 -0400

    Time: 2008 ms (80.47%), Space: 58.3 MB (21.81%) - LeetHub

--- 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.py
+++ 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.py
@@ -11,14 +11,12 @@ class Solution:
                 if start in painted:
                     prev_end = painted[start]
                     painted[start] = max(painted[start], end)
-                    start = prev_end
+                    start = prev_end                    
+                    
                 else:
                     painted[start] = end
                     start += 1
                     work += 1
-                    
-            res.append(work)
             
+            res.append(work)
         return res
-                    
-        

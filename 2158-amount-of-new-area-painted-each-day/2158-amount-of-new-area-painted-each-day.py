cc0af9bbe44125b7c8d32315300413e360d0d0db
Av2939 <41278980+Av2939@users.noreply.github.com>
2022-08-12 00:46:13 -0400

    Time: 2405 ms (62.25%), Space: 62.2 MB (6.20%) - LeetHub

--- 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.py
+++ 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.py
@@ -1,25 +1,24 @@
 class Solution:
     def amountPainted(self, paint: List[List[int]]) -> List[int]:
-        #reminds me a lot of meeting rooms
-        res = []
         painted = {}
+        res = []
         
         for start, end in paint:
             work = 0
             
             while start < end:
+                
                 if start in painted:
                     prev_end = painted[start]
-                    
                     painted[start] = max(painted[start], end)
                     start = prev_end
-                
                 else:
                     painted[start] = end
                     start += 1
                     work += 1
-            
+                    
             res.append(work)
+            
         return res
-    
-    
+                    
+        

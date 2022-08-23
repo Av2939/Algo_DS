adb792aa369a32b1928985e276600643666706cf
Av2939 <41278980+Av2939@users.noreply.github.com>
2022-08-23 14:11:28 -0400

    Time: 165 ms (49.20%), Space: 18.3 MB (47.64%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        Dict = collections.defaultdict(list)
+        
+        for s in strs:
+            Dict[str(sorted(s))].append(s)
+            
+        return Dict.values()

9f02179cf5f6c142926164b03e71069fb4456a2a
Av2939 <41278980+Av2939@users.noreply.github.com>
2022-08-23 02:21:38 -0400

    Time: 2745 ms (20.74%), Space: 17.7 MB (9.54%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
@@ -1,17 +1,17 @@
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        
         lookup = collections.defaultdict(list)
         output = 0
         
         for i,c in enumerate(s):
             lookup[c].append(i)
             
-        
         def bs(lst, i):
             l = 0
             r = len(lst)
             
-            while l < r:
+            while l<r:
                 mid = (l+r)//2
                 
                 if i < lst[mid]:
@@ -20,11 +20,13 @@ class Solution:
                     l = mid + 1
             return l
         
-        
-        
         for w in words:
             prev = -1
             found = True
+        
+        
+        
+        
             
             for c in w:
                 tmp = bs(lookup[c], prev)

ca44fcdb2f694be95bcd4182fa942c6c1026e18d
Av2939 <41278980+Av2939@users.noreply.github.com>
2022-08-24 17:40:24 -0400

    Time: 89 ms (73.03%), Space: 15.3 MB (24.20%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,5 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
         Dict = {}
         
         for i in range(len(nums)):
@@ -7,6 +8,6 @@ class Solution:
             val = target - nums[i]
             
             if val in Dict:
-                return [Dict[val], i]
+                return[Dict[val], i]
             
             Dict[nums[i]] = i

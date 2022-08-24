aa5b09926c14a4208affa3ff78c5ad2ddd825277
Av2939 <41278980+Av2939@users.noreply.github.com>
2022-08-24 04:01:49 -0400

    Time: 909 ms (34.01%), Space: 22.5 MB (12.94%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,16 +1,11 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        #Going to use a BFS approach
-        #The target will be bottom right corner i.e length of row and col
-        #Got to check the four directions for each step, 'up', 'down', 'left', 'right'
-        
         rows, cols = len(grid), len(grid[0])
         target = (rows-1, cols-1)
-        state = (0,0,k) #row, col, obstacles
         
-        seen = set([state])
-        
-        queue = collections.deque([(0,state)])
+        state = (0,0,k) # row, col, elims
+        visited = set([state])
+        queue = collections.deque([(0, state)]) #steps, state
         
         while queue:
             steps, (row, col, k) = queue.popleft()
@@ -18,20 +13,16 @@ class Solution:
             if (row, col) == target:
                 return steps
             
-            #checking all four directions 
             
-            for new_row, new_col in [(row+1, col), (row-1, col), (row, col+1), (row, col-1)]:
+            for new_row, new_col in [(row+1, col), (row-1, col), (row, col+1),(row, col-1)]:
+                if (0<= new_row < rows) and (0 <= new_col < cols):
                 
-                if (0 <= new_row < rows) and (0 <= new_col <cols):
-                    
                     new_eliminations = k - grid[new_row][new_col]
                     new_state = (new_row, new_col, new_eliminations)
-                    
-                    if new_eliminations >= 0 and new_state not in seen:
-                        seen.add(new_state)
+
+                    if new_eliminations >= 0 and new_state not in visited:
+                        visited.add(new_state)
                         queue.append((steps+1, new_state))
-            
-        return -1
                     
-            
-        
+                    
+        return -1

6d7c4d72a05769de540f77388d03fd2907cbd6b8
Av2939 <41278980+Av2939@users.noreply.github.com>
2022-08-26 00:10:42 -0400

    Time: 1335 ms (73.08%), Space: 25 MB (86.17%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         maxP = 0
-        
         l = 0
         
         for r in range(1, len(prices)):
-            
             if prices[r] < prices[l]:
                 l = r
             
             maxP = max(maxP, prices[r] - prices[l])
             
         return maxP
+    
+    #This should be O(n) time with O(1) space since the storage is constant.

69b2b09a9879c862b261717ae98f0d87d84fbb8c
Av2939 <41278980+Av2939@users.noreply.github.com>
2022-08-31 18:34:50 -0500

    Time: 148 ms (44.22%), Space: 14.1 MB (31.75%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -4,37 +4,37 @@ class Solution:
         if len(s2) < len(s1):
             return False
         
+        
         s1Count = [0] * 26
         s2Count = [0] * 26
+        
         
         for i in range(len(s1)):
             s1Count[ord(s1[i]) - ord("a")] += 1
             s2Count[ord(s2[i]) - ord("a")] += 1
-        
-        matches = 0        
-        for i in range(26):
             
+        matches = 0
+        for i in range(26):
             if s1Count[i] == s2Count[i]:
                 matches += 1
             else:
                 matches += 0
-                
         
         l = 0
+        
         for r in range(len(s1), len(s2)):
             
             if matches == 26:
                 return True
-            
             
             index = ord(s2[r]) - ord("a")
             s2Count[index] += 1
             
             if s1Count[index] == s2Count[index]:
                 matches += 1
-            
-            elif s1Count[index] +1 == s2Count[index]:
+            elif s1Count[index] + 1 == s2Count[index]:
                 matches -= 1
+            
             
             index = ord(s2[l]) - ord("a")
             s2Count[index] -= 1
@@ -43,7 +43,6 @@ class Solution:
                 matches += 1
             elif s1Count[index] - 1 == s2Count[index]:
                 matches -= 1
-            
             
             l += 1
         return matches == 26

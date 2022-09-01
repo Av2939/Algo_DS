ed8410e7abfd7945b55d5663b2e4c039df0244c5
Av2939 <41278980+Av2939@users.noreply.github.com>
2022-09-01 01:06:47 -0500

    Time: 53 ms (39.12%), Space: 13.8 MB (98.11%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,6 +1,6 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        Map = {")":"(", "]":"[", "}":"{"}
+        Map = {"}":"{", ")":"(", "]":"["}
         stack = []
         
         for c in s:
@@ -8,7 +8,7 @@ class Solution:
             if c not in Map:
                 stack.append(c)
                 continue
-                
+            
             if not stack or stack[-1] != Map[c]:
                 return False
             stack.pop()

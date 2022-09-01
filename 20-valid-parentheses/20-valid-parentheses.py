fcf48f1a6a00c727f23a3a56bf1f785f85169252
Av2939 <41278980+Av2939@users.noreply.github.com>
2022-08-31 23:34:28 -0500

    Time: 35 ms (87.29%), Space: 13.9 MB (72.07%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -8,11 +8,8 @@ class Solution:
             if c not in Map:
                 stack.append(c)
                 continue
-            
+                
             if not stack or stack[-1] != Map[c]:
                 return False
             stack.pop()
-        
         return not stack
-        
-        

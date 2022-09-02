f46b719a97e328e219117b1953142fc540a75e5d
Av2939 <41278980+Av2939@users.noreply.github.com>
2022-09-02 03:35:21 -0500

    Time: 1019 ms (31.45%), Space: 60.8 MB (6.09%) - LeetHub

--- 221-maximal-square/221-maximal-square.py
+++ 221-maximal-square/221-maximal-square.py
@@ -1,25 +1,25 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         rows, cols = len(matrix), len(matrix[0])
-        cache = {}
+        memo = {}
         
-        
-        def helper(r,c):
+        def rec(r,c):
+            
             if r >= rows or c >= cols:
                 return 0
             
-            if (r,c) not in cache:
-                down = helper(r+1, c)
-                right = helper(r, c+1)
-                diag = helper(r+1, c+1)
+            if (r,c) not in memo:
+                right = rec(1+r, c)
+                down = rec(r, c+1)
+                diag = rec(r+1,c+1)
                 
-                cache[(r,c)] = 0
+                memo[(r,c)] = 0
                 
                 if matrix[r][c] == "1":
-                    cache[(r,c)] = 1 + min(down, right, diag)
-            return cache[(r,c)]
-        
-        helper(0,0)
-        return max(cache.values())**2
+                    memo[(r,c)] = 1 + min(right, down, diag)
+            return memo[(r,c)]
         
         
+        
+        rec(0,0)
+        return max(memo.values())**2

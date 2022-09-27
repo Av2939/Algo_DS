9e9535132c8995c694c57be0ac063d005b913776
Av2939 <41278980+Av2939@users.noreply.github.com>
2022-09-27 14:00:36 -0400

    Time: 75 ms (5.00%), Space: 13.7 MB (43.56%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -4,20 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
         res = []
         
-        def backtracking(first = 0, curr = []):
+        def backtrack(first = 0, curr = []):
             
             if len(curr) == k:
                 res.append(curr[:])
                 
+            
             for i in range(first, len(nums)):
                 curr.append(nums[i])
-                backtracking(i+1, curr)
+                backtrack(i+1, curr)
                 curr.pop()
-            
+        
+        
+        
+        
+        
         for k in range(len(nums)+1):
-            backtracking()
+            backtrack()
         
         return res

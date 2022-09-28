964c58bad87273bfa43e825bf9b4a6d3e00aec26
Av2939 <41278980+Av2939@users.noreply.github.com>
2022-09-27 20:55:22 -0400

    Time: 91 ms (88.99%), Space: 17.1 MB (38.92%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,25 +1,19 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
-        Count = {}
-        
-        freq = [[] for _ in range(len(nums) + 1)]
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        elementCount = {}
         
         for num in nums:
-            Count[num] = 1 + Count.get(num, 0)
+            
+            elementCount[num] = 1 + elementCount.get(num, 0)
             
         
-        for n,c in Count.items():
-            freq[c].append(n)
-            
+        pQueue = [[-count, element] for element, count in elementCount.items()]
         
-        res = []
+        heapq.heapify(pQueue)
         
-        
-        for i in range(len(freq)-1, 0, -1):
-            
-            for j in freq[i]:
-                res.append(j)
-            
-            if len(res) == k:
-                return res
+        return [heapq.heappop(pQueue)[1] for i in range(k)]

1b10206570a1388d46d483f69edcd5bdb357e266
Av2939 <41278980+Av2939@users.noreply.github.com>
2022-09-29 05:45:50 -0400

    Time: 34 ms (81.94%), Space: 14 MB (19.27%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        rows, cols = len(matrix), len(matrix[0])
+        
+        top, down = 0, rows-1
+        
+        
+        while top <= down:
+            
+            row = (top + down)//2
+            
+            if target > matrix[row][-1]:
+                top = row + 1
+            
+            elif target < matrix[row][0]:
+                down = row -1
+            else:
+                break
+        if not (top <= down):
+            return False     
+        
+        row = (top+down)//2
+        l,r = 0, cols-1
+        
+        while l <= r:
+            m = (l+r)//2
+            
+            if target < matrix[row][m]:
+                r = m - 1
+            elif target > matrix[row][m]:
+                l = m + 1
+            else:
+                return True
+        
+        return False

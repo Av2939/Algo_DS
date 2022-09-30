d6a20c5bced4510dfe9f48e8299d334aada2ca0f
Av2939 <41278980+Av2939@users.noreply.github.com>
2022-09-30 19:58:24 -0400

    Time: 1225 ms (15.18%), Space: 34.8 MB (9.68%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -4,40 +4,26 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        
-        if not grid:
-            return 0
-        
         rows, cols = len(grid), len(grid[0])
-        visited = set()
         islands = 0
+        visited = set()
         
         
-        def bfs(r,c):
-            
+        def dfs(r,c):
+            if (r not in range(rows) or c not in range(cols)
+                or grid[r][c] == "0" or (r,c) in visited):
+                return 
             visited.add((r,c))
-            queue = collections.deque()
-            queue.append((r,c))
-            
-            while queue:
-                r, c = queue.popleft()
-                
-                directions = [[1,0], [0,1], [-1, 0], [0, -1]]
-                
-                for dr, dc in directions:
-                    row, col = r + dr, c + dc
-                    
-                    if (row in range(rows) and col in range(cols)
-                        and (row,col) not in visited and grid[row][col] == "1"):
-                        queue.append((row,col))
-                        visited.add((row,col))
-                    
+            directions = [(1,0), (0,1), (-1, 0), (0,-1)]
+            for dr, dc in directions:
+                dfs(r+dr, c+dc)
+        
         
         for r in range(rows):
             for c in range(cols):
                 
                 if grid[r][c] == "1" and (r,c) not in visited:
-                    bfs(r,c)
                     islands += 1
+                    dfs(r,c)
                     
         return islands

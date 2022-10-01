cab3ab625416525fab338181778862d7e1a9dd67
Av2939 <41278980+Av2939@users.noreply.github.com>
2022-10-01 02:49:30 -0400

    Time: 105 ms (12.38%), Space: 21.1 MB (12.16%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        nodeSet = set()
+        
+        curr = head
+        
+        while curr:
+            if curr in nodeSet:
+                return True
+            
+            nodeSet.add(curr)
+            
+            curr = curr.next 
+        
+        return False

a31a2ca79287a1831010c019dc145a8b3d2e176a
Av2939 <41278980+Av2939@users.noreply.github.com>
2022-10-04 01:25:14 -0400

    Time: 28 ms (95.25%), Space: 16.2 MB (40.60%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -11,21 +11,18 @@ class Solution(object):
         :rtype: int
         """
         
-        self.diameter = 0
-        self.dfs(root)
-        return self.diameter
-        
-        
-    
-    def dfs(self, node):
-        
-        if node is None:
-            return 0
-        
-        left_max = self.dfs(node.left)
-        right_max = self.dfs(node.right)
-        
-        self.diameter = max(self.diameter, left_max + right_max)
-        
-        return max(left_max, right_max) + 1
-        
+        res= [0]
+
+        def dfs(root):
+
+            if not root:
+                return 0
+            
+            left = dfs(root.left)
+            right = dfs(root.right)
+            res[0] = max(res[0], left + right)
+
+            return 1 + max(left, right)
+
+        dfs(root)
+        return res[0]
